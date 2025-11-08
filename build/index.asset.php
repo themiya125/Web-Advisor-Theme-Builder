b5b724953535d7d6ec47e26bcf657eb7201cf473
Themiya Jayakodi <themiyajayakody125@gmail.com>
2025-11-08 20:57:35 +0530

    up

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => 'f50bfe5075ee65e8e3a0');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => 'abf31e53686ec4b7a1f5');
